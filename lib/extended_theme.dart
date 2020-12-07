b8af96fdf016c9f9a0319e60e282f110441aa716
boris-home <boris-home@home.com>
2020-12-07 16:44:08 +0800

    Error checks and doc

--- lib/extended_theme.dart
+++ lib/extended_theme.dart
@@ -40,21 +40,29 @@ class ExtendedTheme {
 //   }
 // }
 
+/// Widget for managing the application themes.
+/// Wrap in it your root widget to manage the application theme
 class StatefulThemeProvider<TTheme extends ExtendedTheme>
     extends StatefulWidget {
-  final String initialTheme;
+  final String initialThemeId;
   final Map<String, TTheme> availableThemes;
+  final TTheme initialTheme;
   final ThemedWidgetBuilder<TTheme> themeBuilder;
 
   const StatefulThemeProvider(
-      {Key key, this.initialTheme, this.availableThemes, this.themeBuilder})
+      {Key key,
+      this.initialThemeId,
+      this.availableThemes,
+      this.initialTheme,
+      this.themeBuilder})
       : super(key: key);
 
   @override
-  StatefulThemeProviderState<TTheme> createState() =>
-      StatefulThemeProviderState<TTheme>();
+  _StatefulThemeProviderState<TTheme> createState() =>
+      _StatefulThemeProviderState<TTheme>();
 
-  static ThemeFacade<TTheme> of<TTheme extends ExtendedTheme>(
+  /// Link to theme controller
+  static ThemeHolder<TTheme> of<TTheme extends ExtendedTheme>(
       BuildContext context) {
     return context
         .dependOnInheritedWidgetOfExactType<InheritedTheme<TTheme>>()
@@ -63,44 +71,90 @@ class StatefulThemeProvider<TTheme extends ExtendedTheme>
   }
 }
 
-class ThemeFacade<TTheme extends ExtendedTheme> {
-  final StatefulThemeProviderState<TTheme> _facilityState;
+/// A controller that stores a link to theme and allows you to update it
+class ThemeHolder<TTheme extends ExtendedTheme> {
+  final _StatefulThemeProviderState<TTheme> _facilityState;
 
-  ThemeFacade(this._facilityState);
+  ThemeHolder(this._facilityState);
 
+  /// Current theme identifier if you use predefined map of themes
+  /// Equals null if you update theme in runtime by method [updateTheme]
   String get currentThemeId => _facilityState.themeId;
+
+  /// Current theme. Use this in the Widget tree for getting of theme properties
   TTheme get currentTheme => _facilityState.theme;
 
+  /// Update the theme by the identifier that is
+  /// contained in the map of themes you have defined
   updateThemeById(String themeId) {
     _facilityState.updateThemeById(themeId);
   }
+
+  /// Update the theme by theme object
+  updateTheme(TTheme theme) {
+    _facilityState.updateTheme(theme);
+  }
 }
 
-class StatefulThemeProviderState<TTheme extends ExtendedTheme>
+class _StatefulThemeProviderState<TTheme extends ExtendedTheme>
     extends State<StatefulThemeProvider<TTheme>> {
   String _themeId;
-  ThemeFacade<TTheme> _facade;
+  TTheme _theme;
+
+  ThemeHolder<TTheme> _facade;
 
   String get themeId => _themeId;
-  TTheme get theme => widget.availableThemes[_themeId];
+  TTheme get theme =>
+      _themeId != null ? widget.availableThemes[_themeId] : _theme;
 
-  ThemeFacade<TTheme> get themeFacade => _facade;
+  ThemeHolder<TTheme> get themeFacade => _facade;
 
-  updateThemeById(String newTheme) {
-    if (_themeId != newTheme) {
-      setState(() {
-        _themeId = newTheme;
-        debugPrint("Updated theme: " + newTheme.toString());
-      });
-      //notifyListeners();
+  void _checkThemeId(String newThemeId) {
+    if (!widget.availableThemes.containsKey(newThemeId)) {
+      throw Exception("Themes does not contains this key!");
     }
+  }
+
+  updateThemeById(String newThemeId) {
+    if (_themeId != newThemeId) {
+      _checkThemeId(newThemeId);
+
+      setState(() {
+        _themeId = newThemeId;
+        debugPrint("Updated theme: " + newThemeId.toString());
+      });
+    }
+  }
+
+  updateTheme(TTheme theme) {
+    setState(() {
+      _themeId = null;
+      _theme = theme;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    _facade = ThemeFacade(this);
-    _themeId = widget.initialTheme;
+
+    if (widget.initialThemeId != null && widget.initialTheme != null) {
+      throw Exception(
+          "It is not allowed to specify both the identifier and an instance of the theme!");
+    }
+
+    if (widget.initialThemeId != null) {
+      if (widget.availableThemes == null) {
+        throw Exception(
+            "When passing an identifier, you need to specify the map of themes!");
+      } else {
+        _checkThemeId(widget.initialThemeId);
+      }
+    }
+
+    _facade = ThemeHolder(this);
+
+    _themeId = widget.initialThemeId;
+    _theme = widget.initialTheme;
   }
 
   @override
@@ -117,7 +171,7 @@ class StatefulThemeProviderState<TTheme extends ExtendedTheme>
 }
 
 class InheritedTheme<TTheme extends ExtendedTheme> extends InheritedWidget {
-  final StatefulThemeProviderState<TTheme> stateTheme;
+  final _StatefulThemeProviderState<TTheme> stateTheme;
 
   InheritedTheme({Key key, this.stateTheme, Widget child})
       : super(key: key, child: child);
