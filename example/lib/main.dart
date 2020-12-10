2cbc04591840aa82bfc4e80101854f0cfa1cdad3
boris-home <boris-home@home.com>
2020-12-10 20:23:30 +0800

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,19 +4,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'theme.dart';
 
-// typedef ThemedWidgetBuilder<TTheme extends ExtendedTheme> = Widget Function(
-//     BuildContext context, TTheme theme);
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// Read last theme from [SharedPreferences] for simple example
   /// Avoid [async] in main method for production. Try to use FutureBuilder or other async widgets
   final initialTheme = await _readLastTheme();
-  //final controller = ThemeController<AppTheme>(initialTheme, appThemes);
 
   runApp(MyApp(
-    //themeController: controller,
     initialTheme: initialTheme,
   ));
 }
@@ -29,11 +24,10 @@ Future<String> _readLastTheme() async {
 }
 
 class MyApp extends StatelessWidget {
-  //final ThemeController<AppTheme> themeController;
   final String initialTheme;
 
   const MyApp({Key key, this.initialTheme}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return ThemeScope<AppTheme>(
@@ -44,8 +38,7 @@ class MyApp extends StatelessWidget {
 
         return MaterialApp(
           title: 'Flutter Demo',
-          theme: appTheme
-              .material, // appTheme.materialTheme, //context.t2().theme.materialTheme,
+          theme: appTheme.material,
           home: MyHomePage(title: 'Flutter Demo Home Page'),
         );
       },
@@ -55,16 +48,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -76,47 +59,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -132,11 +86,6 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         backgroundColor:
             ThemeHolder.themeOf<AppTheme>(context).buttonPauseColor,
-
-        // context
-        //     .t2()
-        //     .theme
-        //     .buttonPauseColor, //Theme.of(context).primaryColor,
         onPressed: () {
           final themeKeys = appThemes.keys.toList();
           ThemeHolder.of<AppTheme>(context)
